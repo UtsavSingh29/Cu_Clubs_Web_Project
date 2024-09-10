96bb0259fd9b484c46784f0925742b883766290b
UtsavSingh29 <kutsavsingh>
2024-09-10 15:10:45 +0530

    updatedtheoverall look and buttons

---
+++ login/contact/app.js
@@ -0,0 +1,22 @@
+const inputs = document.querySelectorAll(".input");
+
+function focusFunc() {
+  let parent = this.parentNode;
+  parent.classList.add("focus");
+}
+
+function blurFunc() {
+  let parent = this.parentNode;
+  if (this.value == "") {
+    parent.classList.remove("focus");
+  }
+}
+
+inputs.forEach((input) => {
+  input.addEventListener("focus", focusFunc);
+  input.addEventListener("blur", blurFunc);
+});
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+            event.preventDefault();  // Prevents the form from submitting normally
+            alert("Your message has been sent!");
+});
